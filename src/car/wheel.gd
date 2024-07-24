78d4232451f2ca777785d909d6a82a59a295ccd5
Teddy-Desktop <teddy.sauyet@gmail.com>
2024-07-23 21:17:16 -0400

    Added skeleton of wheel class. Need to figure out how to actually do physics lol. Then write the response function.

---
+++ src/car/wheel.gd
@@ -0,0 +1,39 @@
+class_name Wheel
+
+## This class models a single wheel responsible for a particular mass
+
+## Wheel parameters
+
+## the mass in kg pushing down on this wheel (straight down)
+@export var weight : float = 1.0
+## friction coefficients
+@export var static_friction : float = 0.9
+@export var rolling_friction : float = 0.01
+@export var kinetic_friction : float = 0.5
+## the value of gravity. m/s
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+## state variables
+
+## COM speed, m/s
+var linear_speed : float = 0.0
+## angle of the wheel wrt to the COM velocity (0 = aligned perfectly, radians)
+var angle : float = 0.0
+#i think that's all we need.
+### how fast the wheel is spinning 
+#var angular_velocity : float = 0.0
+#enum FrictionMode {
+	#KINETIC,
+	#STATIC,
+#}
+#var friction_mode : FrictionMode = FrictionMode.STATIC
+
+## returns the current force (not normalized for delta or anything)
+## x is the forward (trunk to hood) direction, and y is transverse direction
+func calculate_response_force(applied_force : Vector2) -> Vector2:
+	var result : Vector2 = Vector2.ZERO
+	
+	#calculate transverse force
+	var normal_force : float = weight * gravity
+	var max_transverse_force : float = static_friction * normal_force
+	if max_transverse_force >= applied_force.y
