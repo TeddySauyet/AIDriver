19ccfe636ec3c51f6c3235ecd1050c43c994d0a7
Teddy Sauyet (surface laptop) <teddy.sauyet@gmail.com>
2024-07-25 13:30:26 -0400

    First draft of the wheel.

--- src/car/wheel.gd
+++ src/car/wheel.gd
@@ -5,7 +5,7 @@ class_name Wheel
 ## Wheel parameters
 
 ## the mass in kg pushing down on this wheel (straight down)
-@export var weight : float = 1.0
+@export var mass : float = 1.0
 ## friction coefficients
 @export var static_friction : float = 0.9
 @export var rolling_friction : float = 0.01
@@ -15,28 +15,29 @@ var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 ## state variables
 
-## wheel velocity, m/s
-var velocity : Vector2 = Vector2.ZERO
+## tire speed - tangetial speed of the tire
+var tire_speed : float = 0.0 #not implemented yet
 ## angle of the wheel wrt to the velocity (0 = aligned perfectly, radians)
 var angle : float = 0.0
-#i think that's all we need.
-### how fast the wheel is spinning 
-#var angular_velocity : float = 0.0
-#enum FrictionMode {
-	#KINETIC,
-	#STATIC,
-#}
-#var friction_mode : FrictionMode = FrictionMode.STATIC
 
 ## returns the current force (not normalized for delta or anything)
-func calculate_response_force(applied_force : Vector2) -> Vector2:
-	var result : Vector2 = Vector2.ZERO
-	
-	#calculate transverse force
-	var normal_force : float = weight * gravity
-	var max_transverse_force : float = static_friction * normal_force
-	var drifting : bool = max_transverse_force < applied_force.y
+## current_velocity (v_forward, v_sideways)
+## applied torque - positive for driving forward
+## applied braking is > 0
+func calculate_response_force(velocity : Vector2, 
+		driving_force : float, applied_braking : float) -> Vector2:
+	var orientation := Vector2(1,0).rotated(angle)
+	var transverse_direction := Vector2(1,0).rotated(90+angle)
+	var transverse_force := velocity.project(transverse_direction)
+	var drifting : bool = transverse_force.length() > static_friction*mass*gravity
 	if drifting:
-		return applied_force - velocity.normalized() * normal_force * kinetic_friction
+		var friction := -velocity.normalized() * mass*gravity*kinetic_friction
+		#do this later probably
+		#var wheel_inertia_force := orientation * 
+		return friction
 	else:
-		var transverse_force = 
+		var friction := -orientation * mass * gravity * rolling_friction
+		var driving := orientation * driving_force
+		var braking = -orientation * abs(applied_braking)
+		return transverse_force + friction + driving+braking
+	
