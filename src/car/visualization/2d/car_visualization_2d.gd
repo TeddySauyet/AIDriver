870a02a55608a07d542de3d386fbeb8947886555
Teddy-Desktop <teddy.sauyet@gmail.com>
2024-08-25 19:51:14 -0400

    Project runs, time to add some code

--- src/car/visualization/2d/car_visualization_2d.gd
+++ src/car/visualization/2d/car_visualization_2d.gd
@@ -4,11 +4,7 @@ extends Node2D
 @export var car : Car
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$wheel_visualization.wheel = car.wheel_df
-	$wheel_visualization2.wheel = car.wheel_pf
-	$wheel_visualization3.wheel = car.wheel_dr
-	$wheel_visualization4.wheel = car.wheel_pr
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
