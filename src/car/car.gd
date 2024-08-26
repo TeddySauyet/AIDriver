0283649a549b8394451ba3ad6225b6eb3535a394
Teddy-Desktop <teddy.sauyet@gmail.com>
2024-08-25 20:18:31 -0400

    Driving around normally works, gotta add drift and low-speed turn rules

--- src/car/car.gd
+++ src/car/car.gd
@@ -1,3 +1,20 @@
 class_name Car
 extends CharacterBody2D
 
+var acceleration : float = 0.64*5
+var drag : float = 0.05
+
+var turn_sensitivity : float = PI/3.0
+
+
+func _process(delta: float) -> void:
+	var turn = Input.get_axis('a','d') * turn_sensitivity * delta
+	transform = transform.rotated_local(turn)
+	
+	var input_accel : float = Input.get_axis("s","w") * delta * acceleration
+
+	var drag : float = velocity.length_squared() * drag * delta
+	velocity += input_accel * transform.x
+	velocity -= drag * velocity.normalized()
+	move_and_collide(velocity)
+	print(velocity.length())
