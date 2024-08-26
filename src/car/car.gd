66ea3137dbd471c79788a48e3376ecb5bcb40941
Teddy-Desktop <teddy.sauyet@gmail.com>
2024-08-25 20:57:20 -0400

    Dotta calculate desired direction/speed then calculate net acel based on this

--- src/car/car.gd
+++ src/car/car.gd
@@ -3,18 +3,34 @@ extends CharacterBody2D
 
 var acceleration : float = 0.64*5
 var drag : float = 0.05
-
 var turn_sensitivity : float = PI/3.0
 
+var max_acceleration_before_drift := 3.0
+
+enum State {
+	NORMAL,
+	DRIFTING,
+}
+
+var state : State = State.NORMAL
 
 func _process(delta: float) -> void:
-	var turn = Input.get_axis('a','d') * turn_sensitivity * delta
-	transform = transform.rotated_local(turn)
-	
-	var input_accel : float = Input.get_axis("s","w") * delta * acceleration
+	var net_acceleration : Vector2
+	var drag : Vector2 = -velocity.length_squared() * drag * delta*velocity.normalized()
+	match state:
+		State.NORMAL:
+			var turn = Input.get_axis('a','d') * turn_sensitivity * delta
+			transform = transform.rotated_local(turn)
+			
+			var input_accel : float = Input.get_axis("s","w") * delta * acceleration
+			net_acceleration= input_accel * transform.x + drag
+			
+			velocity += net_acceleration
 
-	var drag : float = velocity.length_squared() * drag * delta
-	velocity += input_accel * transform.x
-	velocity -= drag * velocity.normalized()
+		State.DRIFTING:
+			velocity += drag
+	if net_acceleration.length() > max_acceleration_before_drift:
+		state = State.DRIFTING
+	#print(velocity.length())
 	move_and_collide(velocity)
-	print(velocity.length())
+	
